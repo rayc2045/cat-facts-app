4b558d8532b49e80e324c770a2a0883ee25f4f02
rayc2045 <>
2021-09-14 22:37:48 +0800

    Add components

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HeaderLink from './HeaderLink.vue'
+
+export default defineComponent({
+  components: { HeaderLink }
+})
+</script>
+
+<template>
+  <div class="my-4 text-center">
+    <h4>🐱 Cat Facts</h4>
+    <div class="d-flex justify-content-center">
+      <HeaderLink to="/" exact>Home</HeaderLink>
+      <HeaderLink to="/facts">List</HeaderLink>
+    </div>
+  </div>
+</template>
---
+++ src/components/HeaderLink.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+  props: {
+    to: { type: String, required: true },
+    exact: { type: Boolean, required: false }
+  },
+  setup(props) {
+    const route = useRoute()
+    const active = computed(() => {
+      props.exact
+        ? route.path === props.to
+        : route.path.startsWith(props.to)
+    })
+
+    return { active }
+  }
+})
+</script>
+
+<template>
+  <div style="width: 150px">
+    <router-link
+      :to="to"
+      class="nav-link"
+      :class="active ? 'font-weight-bold': null"
+    >
+      <slot />
+    </router-link> 
+  </div>
+</template>
